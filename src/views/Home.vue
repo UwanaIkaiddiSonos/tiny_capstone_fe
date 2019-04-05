106400f4ac9129e009d33033807c31352c16cd3a
Uwana <uwikaiddi@gmail.com>
2019-04-04 19:28:13 -0500

    Add show

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <input type="text">
-    <button v-on:click="addNewProduct">Add a new product</button>
+    <p>Name: <input type="text" v-model="newProductName"></p>
+    <p>Price: <input type="text" v-model="newProductPrice"></p>
+    <p>Description: <input type="text" v-model="newProductDescription"></p>
+    <button v-on:click="addNewProduct()">Add a new product</button>
+
     <div v-for="product in products">
       <p>{{ product.name }}</p>
       <p>{{ product.price }}</p>
-      <p>{{ product.description }}</p>
+      <!--<p>{{ product.description }}</p>-->
       <p><img v-bind:src="product.image_url"></p>
+      <div v-if="product === currentProduct">
+        <p>{{ product.description}}</p>
+      </div>
+      <button v-on:click="currentProduct = product">Show more info</button>
       <hr>
     </div>
   </div>
 </template>
 
 <style>
+  img {
+    width: 80px;
+  }
 </style>
 
 <script>
@@ -21,8 +31,12 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
-      products: []
+      message: "Hello to Vue.js!",
+      products: [],
+      newProductName: "",
+      newProductPrice: "",
+      newProductDescription: "",
+      currentProduct: {}
     };
   },
   created: function() {
@@ -30,15 +44,20 @@ export default {
       this.products = response.data;
     })
   },
-  methods: {
+   methods: {
     addNewProduct: function() {
-      console.log('add new product')
+      console.log('adding new product...');
       var params = {
-        name: "painting",
-        price: 40,
-        description: "by VanGogh",
-        image_url: "#"
+        input_name: this.newProductName,
+        input_price: this.newProductPrice,
+        input_description: this.newProductDescription
       }
+      console.log(params);
+      // send the data to the API
+      axios.post("/api/products", params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+      });
     }
   }
 };
